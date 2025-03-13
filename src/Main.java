2fafd22337861df6681d92c55d5924d753229551
CodeFather. <a88869113@gmail.com>
2025-03-13 04:44:31 +0300

    Upload All Files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import GUI.*;
+import javax.swing.*;
+
+public class Main {
+    /*
+    * Telegram: https://t.me/CodeFather_xe
+    * Instagram: https://www.instagram.com/_codefather/
+    * */
+    public static void main(String[] args) {
+SwingUtilities.invokeLater(MainFrame::new);
+    }
+}
